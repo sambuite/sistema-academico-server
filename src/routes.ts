7b937017ff6e4b344c188af71484b7c7c4865593
sambuite <murilosambuite@gmail.com>
2021-04-09 18:28:04 -0300

    Added professor routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import SessionController from '@controllers/SessionController';
 import { UsersController } from '@controllers/UserController';
+import { ProfessorController } from '@controllers/ProfessorController';
 import { Router } from 'express';
 import auth from './middlewares/auth';
 
 const userController = new UsersController();
 const sessionController = new SessionController();
+const professorController = new ProfessorController();
 
 const router = Router();
 
@@ -12,12 +14,16 @@ router.post('/login', sessionController.create);
 
 router.post('/register-user', userController.create);
 
+router.post('/register-professor', professorController.create);
+
 router.use(auth);
 
 router.get('/users', userController.index);
-
 router.put('/edit-user/:id', userController.edit);
-
 router.delete('/delete-user/:id', userController.delete);
+
+router.get('/professors', professorController.index);
+router.put('/edit-professor/:id', professorController.edit);
+router.delete('/delete-professor/:id', professorController.delete);
 
 export { router };
